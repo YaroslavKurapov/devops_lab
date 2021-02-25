9dcb5f6643344db12c553c7e0883c32edc2c2fc0
Yaroslav_Kurapov <Yaroslav@MacBook-Air-Aliaksandra.local>
2021-02-25 21:45:07 +0300

    Homework3: Yaroslav Kurapov

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-DevOps Lab 2021 winter/spring
+!!!!!Python monitoring app!!!!!
+
+This app create file 'result' in which provide information about the system every 5 minutes, such as:
+CPU load
+Memory usage
+Virtual memory usage
+IO information
+Network information.
+
+##ALL INFORMATION EXEPT CPU LOAD(%) AND NETWORK INFORMATION(STRING) PROVIDED IN BYTES##
+
+!!!To install this app, download folder with package, execute command 'pip install .' from this directory!!!
+!!!Use command 'python snapshot' to run the app!!!
+!!!Use command 'python snapshot --type=json' to see output in json format in file 'result'!!!
+!!!By default output in simple text format!!!
---
+++ main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import psutil
+import json
+import time
+from datetime import datetime
+import argparse
+
+parser = argparse.ArgumentParser(description='My example explanation')
+parser.add_argument(
+    '--time',
+    type=int,
+    default=300,
+    help='time (default: 2)'
+)
+parser.add_argument(
+    '--type',
+    type=str,
+    default='text',
+    help='text or json'
+)
+my_namespace = parser.parse_args()
+
+
+def system_info():
+    cpu_overall = [x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]
+    memory = psutil.virtual_memory()[0]
+    virtual_memory = psutil.virtual_memory()[3]
+    io = psutil.disk_io_counters()[0:2]
+    network = psutil.net_if_addrs()['lo0'][0]
+    snapshot = {'cpu_overall': cpu_overall,
+                'total memory': memory,
+                'virtual memory': virtual_memory,
+                'IO': io,
+                'network': network,
+                }
+    return snapshot
+
+
+def display(info, n):
+    snapshot = info
+
+    result = (f"SNAPSHOT {n} : {datetime.now()}\n"
+              f"CPU overall load     | {snapshot['cpu_overall']}\n"
+              f"Memory usage         | {snapshot['total memory']}\n"
+              f"Virtual memory usage | {snapshot['virtual memory']}\n"
+              f"IO                   | {snapshot['IO']}\n"
+              f"Network              | {snapshot['network']}\n")
+    return result
+
+
+def display_json(info, n):
+    snapshot = info
+    result = f"SNAPSHOT {n} : {datetime.now()}\n" \
+             f"{json.dumps(snapshot)}\n"
+    return result
+
+
+def output():
+    interval = my_namespace.time
+    file_type = my_namespace.type
+    info = system_info()
+    counter = 1
+    while True:
+        if file_type == 'text':
+            with open('result', 'a') as file:
+                file.writelines(display(info=info, n=counter) + '\n')
+        elif file_type == 'json':
+            with open('result', 'a') as file:
+                file.writelines(display_json(info=info, n=counter) + '\n')
+        time.sleep(interval)
+        counter += 1
+
+
+if __name__ == "__main__":
+    output()
