00ac19138620381838ee003a4dbe3f56a19b89f4
Yaroslav_Kurapov <Yaroslav@MacBook-Air-Aliaksandra.local>
2021-02-25 21:57:03 +0300

    final fix setup.py file syntax

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'snapshot',
-    version = '0.1.0',
-    scripts = ['main.py'],
-    packages = find_packages(),
-    author = 'Yaroslav Kurapov'
+    name='snapshot',
+    version='0.1.0',
+    scripts=['main.py'],
+    packages=find_packages(),
+    author='Yaroslav Kurapov'
 )
