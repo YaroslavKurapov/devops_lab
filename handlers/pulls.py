8b37ab606dbbbe855e217666c3eea59089ce9f74
Yaroslav_Kurapov <Yaroslav@MacBook-Air-Aliaksandra.local>
2021-03-02 16:16:13 +0300

    fix style of code

--- handlers/pulls.py
+++ handlers/pulls.py
@@ -11,17 +11,13 @@ url2 = f'https://api.github.com/search/issues?q=is:pr%20label:"needs%20work"\
 
 
 def get_pulls(state):
-     pull = requests.get(url_pulls)
-
-     if state == "closed" or state == "open":
-         pull = requests.get(url_pulls, params={'state': '{0}'.format(state), 'per_page': 100})
-
-     if state == "needs work":
-         pull = requests.get(url2, params=per_page)
-         return pull.json()["items"]
-
-     if state == "accepted":
-         pull = requests.get(url1, params=per_page)
-         return pull.json()["items"]
-
-     return pull.json()
+    pull = requests.get(url_pulls)
+    if state == "closed" or state == "open":
+        pull = requests.get(url_pulls, params={'state': '{0}'.format(state), 'per_page': 100})
+    if state == "needs work":
+        pull = requests.get(url2, params=per_page)
+        return pull.json()["items"]
+    if state == "accepted":
+        pull = requests.get(url1, params=per_page)
+        return pull.json()["items"]
+    return pull.json()
