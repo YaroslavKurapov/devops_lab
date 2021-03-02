9400a8482c822a7e486c07f6c6eb50a8c037cbdf
Yaroslav_Kurapov <Yaroslav@MacBook-Air-Aliaksandra.local>
2021-03-02 21:23:10 +0300

    fix: show numbers and working links

--- handlers/pulls.py
+++ handlers/pulls.py
@@ -3,21 +3,23 @@ import requests
 owner = "alenaPy"
 repo = "devops_lab"
 per_page = 100
-url_pulls = f'https://api.github.com/repos/{owner}/{repo}/pulls'
-url1 = f'https://api.github.com/search/issues?q=is:pr%20label:"accepted"\
-                    %20repo:{owner}/{repo}&per_page={per_page}'
-url2 = f'https://api.github.com/search/issues?q=is:pr%20label:"needs%20work"\
-                  %20repo:{owner}/{repo}&per_page={per_page}'
 
 
 def get_pulls(state):
-    pull = requests.get(url_pulls)
-    if state == "closed" or state == "open":
-        pull = requests.get(url_pulls, params={'state': '{0}'.format(state), 'per_page': 100})
-    if state == "needs work":
-        pull = requests.get(url2, params=per_page)
-        return pull.json()["items"]
-    if state == "accepted":
-        pull = requests.get(url1, params=per_page)
-        return pull.json()["items"]
-    return pull.json()
+    url_pulls = f'https://api.github.com/repos/{owner}/{repo}/pulls?state={state}&per_page={per_page}'
+    url_main = f'https://api.github.com/search/issues?q=is:pr%20label:\"{state}\"%20repo:{owner}/{repo}&per_page={per_page}'
+    url_all = f'https://api.github.com/search/issues?q=is:pr%20label:"needs%20work"%20repo:{owner}/{repo}&per_page={per_page}'
+    if state in ('open', 'closed'):
+        response = requests.get(url_pulls, params=state)
+        pull = response.json()
+    elif state in ('needs work', 'accepted'):
+        response = requests.get(url_main, params=state)
+        pull = response.json()['items']
+    else:
+        response = requests.get(url_all)
+        pull = response.json()
+
+    output = []
+    for item in pull:
+        output.append({'num': item['number'], 'title': item['title'], 'link': item['html_url']})
+    return output
