cfc0f774f5470aff365a79fa52adc928b6fa6c5c
Yaroslav_Kurapov <Yaroslav@MacBook-Air-Aliaksandra.local>
2021-03-02 23:29:30 +0300

    fix code and tests folder

--- start.py
+++ start.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request
 from handlers.pulls import get_pulls
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def title():
     return 'Show pull requests in devops_lab repo!!'
 
+
 @app.route('/pulls')
 def pulls():
     state = request.args.get("state")
-    return render_template("pulls.j2", pulls = get_pulls(state))
+    return render_template("pulls.j2", pulls=get_pulls(state))
+
 
 app.run()
