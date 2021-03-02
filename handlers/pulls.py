d86cd1869356e7695833a7765d5adc5f5060d7df
Yaroslav_Kurapov <Yaroslav@MacBook-Air-Aliaksandra.local>
2021-03-02 21:34:01 +0300

    fix code last version

--- handlers/pulls.py
+++ handlers/pulls.py
@@ -6,9 +6,12 @@ per_page = 100
 
 
 def get_pulls(state):
-    url_pulls = f'https://api.github.com/repos/{owner}/{repo}/pulls?state={state}&per_page={per_page}'
-    url_main = f'https://api.github.com/search/issues?q=is:pr%20label:\"{state}\"%20repo:{owner}/{repo}&per_page={per_page}'
-    url_all = f'https://api.github.com/search/issues?q=is:pr%20label:"needs%20work"%20repo:{owner}/{repo}&per_page={per_page}'
+    url_pulls = f'https://api.github.com/repos/{owner}/{repo}/pulls?state=\
+                {state}&per_page={per_page}'
+    url_main = f'https://api.github.com/search/issues?q=\
+            is:pr%20label:\"{state}\"%20repo:{owner}/{repo}&per_page={per_page}'
+    url_all = f'https://api.github.com/search/issues?q=\
+         is:pr%20label:"needs%20work"%20repo:{owner}/{repo}&per_page={per_page}'
     if state in ('open', 'closed'):
         response = requests.get(url_pulls, params=state)
         pull = response.json()
@@ -21,5 +24,7 @@ def get_pulls(state):
 
     output = []
     for item in pull:
-        output.append({'num': item['number'], 'title': item['title'], 'link': item['html_url']})
+        output.append({'num': item['number'],
+                       'title': item['title'],
+                       'link': item['html_url']})
     return output
